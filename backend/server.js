7a291ae2a5bcb451342451f057869519a2322413
Vaibhavi <vaibhavichaudhari304@gmail.com>
2025-06-20 13:05:35 +0530

    api

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https:// ',
+    credentials: true
+}));
 app.use(bodyParser.json());
 
 
